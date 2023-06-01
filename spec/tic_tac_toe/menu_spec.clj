52927d681439900245cf699c4e0c4bcbcfe4f9c7
Alex Jensen <alex@endure.io>
2023-06-01 18:23:18 -0400

    menus map to next state

--- spec/tic_tac_toe/menu_spec.clj
+++ spec/tic_tac_toe/menu_spec.clj
@@ -9,10 +9,10 @@
         (with-out-str (render {:state :main-menu}))))
 
     (it "selects new game"
-      (should= {:state :mode-menu} (next-state {:state :main-menu} 1)))
+      (should= {:state :mode-menu} (next-state {:state :main-menu} "1")))
 
     (it "selects replay game"
-      (should= {:state :replay-menu} (next-state {:state :main-menu} 2)))
+      (should= {:state :replay-menu} (next-state {:state :main-menu} "2")))
 
     (it "displays menu again with bad input"
       (should= {:state :main-menu} (next-state {:state :main-menu} nil))))
@@ -23,27 +23,87 @@
         (with-out-str (render {:state :mode-menu}))))
 
     (it "selects pvp game"
-      (should= {:state :pvp-menu} (next-state {:state :mode-menu} 1)))
+      (should= {:state :pvp-menu} (next-state {:state :mode-menu} "1")))
 
     (it "selects pvc game"
-      (should= {:state :pvc-menu} (next-state {:state :mode-menu} 2)))
+      (should= {:state :difficulty-menu} (next-state {:state :mode-menu} "2")))
 
     (it "selects cvc game"
-      (should= {:state :cvc-menu} (next-state {:state :mode-menu} 3)))
+      (should= {:state :difficulty-menu} (next-state {:state :mode-menu} "3")))
 
     (it "displays menu again with bad input"
       (should= {:state :mode-menu} (next-state {:state :mode-menu} nil))))
 
+  (describe "difficulty menu"
+    (it "displays difficulty options"
+      (should= "1) Hard (unbeatable)\n2) Medium\n3) Easy\n"
+        (with-out-str (render {:state :difficulty-menu}))))
+
+    (it "selects hard difficulty"
+      (should= {:state :hard-menu} (next-state {:state :difficulty-menu} "1")))
+
+    (it "selects medium difficulty"
+      (should= {:state :med-menu} (next-state {:state :difficulty-menu} "2")))
+
+    (it "selects easy difficulty"
+      (should= {:state :easy-menu} (next-state {:state :difficulty-menu} "3")))
+
+    (it "displays menu again with bad input"
+      (should= {:state :difficulty-menu} (next-state {:state :difficulty-menu} nil))))
+
+  ; TODO reduce duplication in game menu tests
   (describe "pvp menu"
     (it "displays size options"
       (should= "1) 3x3\n2) 4x4\n"
         (with-out-str (render {:state :pvp-menu}))))
 
     (it "selects 3x3 game"
-      (should= {:state :3x3-game} (next-state {:state :pvp-menu} 1)))
+      (should= :3x3-game (:state (next-state {:state :pvp-menu} "1"))))
 
     (it "selects 4x4 game"
-      (should= {:state :4x4-game} (next-state {:state :pvp-menu} 2)))
+      (should= :4x4-game (:state (next-state {:state :pvp-menu} "2"))))
 
     (it "displays menu again with bad input"
-      (should= {:state :pvp-menu} (next-state {:state :pvp-menu} nil)))))
+      (should= :pvp-menu (:state (next-state {:state :pvp-menu} nil)))))
+
+  (describe "hard difficulty menu"
+    (it "displays size options"
+      (should= "1) 3x3\n2) 4x4\n"
+        (with-out-str (render {:state :hard-menu}))))
+
+    (it "selects 3x3 game"
+      (should= :3x3-game (:state (next-state {:state :hard-menu} "1"))))
+
+    (it "selects 4x4 game"
+      (should= :4x4-game (:state (next-state {:state :hard-menu} "2"))))
+
+    (it "displays menu again with bad input"
+      (should= {:state :hard-menu} (next-state {:state :hard-menu} nil))))
+
+  (describe "medium difficulty menu"
+    (it "displays size options"
+      (should= "1) 3x3\n2) 4x4\n"
+        (with-out-str (render {:state :med-menu}))))
+
+    (it "selects 3x3 game"
+      (should= :3x3-game (:state (next-state {:state :med-menu} "1"))))
+
+    (it "selects 4x4 game"
+      (should= :4x4-game (:state (next-state {:state :med-menu} "2"))))
+
+    (it "displays menu again with bad input"
+      (should= {:state :med-menu} (next-state {:state :med-menu} nil))))
+
+  (describe "easy difficulty menu"
+    (it "displays size options"
+      (should= "1) 3x3\n2) 4x4\n"
+        (with-out-str (render {:state :easy-menu}))))
+
+    (it "selects 3x3 game"
+      (should= :3x3-game (:state (next-state {:state :easy-menu} "1"))))
+
+    (it "selects 4x4 game"
+      (should= :4x4-game (:state (next-state {:state :easy-menu} "2"))))
+
+    (it "displays menu again with bad input"
+      (should= {:state :easy-menu} (next-state {:state :easy-menu} nil)))))
--- src/tic_tac_toe/menu.clj
+++ src/tic_tac_toe/menu.clj
@@ -1,13 +1,21 @@
 (ns tic-tac-toe.menu)
 
+(def size-opts [{:name :3x3-game     :label "3x3"}
+                {:name :4x4-game     :label "4x4"}])
+
 (def menus
-  {:main-menu [{:name :mode-menu    :label "New Game"}
-               {:name :replay-menu  :label "Replay Game"}]
-   :mode-menu [{:name :pvp-menu     :label "Player v. Player"}
-               {:name :pvc-menu     :label "Player v. Computer"}
-               {:name :cvc-menu     :label "Computer v. Computer"}]
-   :pvp-menu  [{:name :3x3-game     :label "3x3"}
-               {:name :4x4-game     :label "4x4"}]})
+  {:main-menu       [{:name :mode-menu        :label "New Game"}
+                     {:name :replay-menu      :label "Replay Game"}]
+   :mode-menu       [{:name :pvp-menu         :label "Player v. Player"}
+                     {:name :difficulty-menu  :label "Player v. Computer"}
+                     {:name :difficulty-menu  :label "Computer v. Computer"}]
+   :difficulty-menu [{:name :hard-menu        :label "Hard (unbeatable)"}
+                     {:name :med-menu         :label "Medium"}
+                     {:name :easy-menu        :label "Easy"}]
+   :pvp-menu        size-opts
+   :hard-menu       size-opts
+   :med-menu        size-opts
+   :easy-menu       size-opts})
 
 (defn fmt-label [idx option] (str (inc idx) ") " option \newline))
 
@@ -17,7 +25,7 @@
 
 (defn select-opt [options selection default-state]
   (let [opt-map (->> (map #(hash-map :state %) options)
-                  (map-indexed (fn [idx opt] {(inc idx) opt}))
+                  (map-indexed (fn [idx opt] {(str (inc idx)) opt}))
                   (into (sorted-map)))]
     (get opt-map selection default-state)))
 
@@ -29,3 +37,10 @@
 
 (defn next-state [cur-state selection]
   (select-opt (select-of cur-state :name) selection cur-state))
+
+(defn menu-loop []
+  (loop [state {:state :main-menu}]
+    (render state)
+    (flush)
+    (let [input (read-line)]
+      (recur (next-state state input)))))
