4886df230518c096a8aff9d6c4eaac703c6e051c
Alex Jensen <alex@endure.io>
2023-06-01 23:44:18 -0400

    Revert "second render refactor"
    This reverts commit 48cebdd1

--- src/tic_tac_toe/menu.clj
+++ src/tic_tac_toe/menu.clj
@@ -1,56 +1,32 @@
-(ns tic-tac-toe.menu
-  (:require [clojure.string :as s]))
+(ns tic-tac-toe.menu)
 
-(def size-opts [{:name :3x3-game     :label "3x3"}
-                {:name :4x4-game     :label "4x4"}])
+(defmulti render :state)
 
-(def menus
-  {:main-menu       [{:name :mode-menu        :label "New Game"}
-                     {:name :replay-menu      :label "Replay Game"}]
-   :mode-menu       [{:name :pvp-menu         :label "Player v. Player"}
-                     {:name :difficulty-menu  :label "Player v. Computer"}
-                     {:name :difficulty-menu  :label "Computer v. Computer"}]
-   :difficulty-menu [{:name :hard-menu        :label "Hard (unbeatable)"}
-                     {:name :med-menu         :label "Medium"}
-                     {:name :easy-menu        :label "Easy"}]
-   :pvp-menu        size-opts
-   :hard-menu       size-opts
-   :med-menu        size-opts
-   :easy-menu       size-opts})
+(defn fmt-opt [idx option] (str (inc idx) ") " option \newline))
 
-(defn fmt-label [idx option] (str (inc idx) ") " option \newline))
-
-(defn fmt-labels [options]
-  (->> (map-indexed fmt-label options)
+(defn fmt-opts [options]
+  (->> (map-indexed fmt-opt options)
     (apply str)))
 
-(defn get-mode [menu]
-  (-> (name menu)
-    (s/replace #"-menu" (str))
-    (keyword)))
+(defmethod render :main-menu [state]
+  (print (fmt-opts ["New Game" "Replay Game"])))
+
+(defmethod render :mode-menu [state]
+  (print (fmt-opts ["Player v. Player" "Player v. Computer" "Computer v. Computer"])))
+
+(defmethod render :pvp-menu [state]
+  (print (fmt-opts ["3x3" "4x4"])))
 
 (defn select-opt [options selection default-state]
   (let [opt-map (->> (map #(hash-map :state %) options)
-                  (map-indexed (fn [idx opt] {(str (inc idx)) opt}))
+                  (map-indexed (fn [idx opt] {(inc idx) opt}))
                   (into (sorted-map)))]
     (get opt-map selection default-state)))
 
-(defn select-of [state key]
-  (map key ((:state state) menus)))
-
-(defn render [cur-state]
-  (print (fmt-labels (select-of cur-state :label))))
+(def menus
+  {:main-menu [:mode-menu :replay-menu]
+   :mode-menu [:pvp-menu :pvc-menu :cvc-menu]
+   :pvp-menu  [:3x3-game :4x4-game]})
 
 (defn next-state [cur-state selection]
-  (let [state (select-opt (select-of cur-state :name) selection cur-state)]
-    (if (= cur-state state)
-      state
-      (assoc state :mode (get-mode (:state cur-state))))))
-
-(defn menu-loop []
-  (loop [state {:state :main-menu}]
-    (print state)
-    (render state)
-    (flush)
-    (let [input (read-line)]
-      (recur (next-state state input)))))
+  (select-opt ((:state cur-state) menus) selection cur-state))
